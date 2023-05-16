7a42f505b2cd31b5c19dadf191ae494e9c1c2bfc
Lucas Grandolpho <lucas.grandolpho@hotmail.com>
2023-05-16 00:07:39 -0300

    feat: frontend
    * feat: init frontend
    
    * feat: tailwind, daisyui
    
    * feat: filepicker, validate, update

---
+++ frontend/src/App.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+
+function App() {
+  const [csvFile, setCsvFile] = useState<File | null>(null);
+  const [isValid, setIsValid] = useState(false);
+  const [validationErrs, setValidationErrs] = useState(null);
+  const [products, setProducts] = useState(null);
+
+  const handleUpdate = async () => {
+    if (!csvFile) return;
+    const csvContent = await csvFile.text();
+    const res = await fetch('http://localhost:3000/products/update', {
+      method: 'POST',
+      mode: 'cors',
+      body: csvContent,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    });
+  };
+
+  const handleValidation = async () => {
+    if (!csvFile) return;
+    const csvContent = await csvFile.text();
+    const res = await fetch('http://localhost:3000/products/validate', {
+      method: 'POST',
+      mode: 'cors',
+      body: csvContent,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+    });
+
+    if (res.status === 400) {
+      setIsValid(false);
+      const body = await res.json();
+      setValidationErrs(body);
+    }
+
+    if (res.status === 200) {
+      setIsValid(true);
+    }
+  };
+
+  const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+    setCsvFile(e.target.files[0]);
+  };
+
+  return (
+    <div>
+      <h1>Atualizar Preços</h1>
+      <input
+        type="file"
+        className="file-input w-full max-w-xs"
+        onChange={handleUpload}
+      />
+      <button
+        disabled={!csvFile}
+        className="btn btn-primary"
+        onClick={handleValidation}
+      >
+        Validar
+      </button>
+      <button
+        className="btn btn-secondary"
+        onClick={handleUpdate}
+        disabled={!isValid}
+      >
+        Atualizar
+      </button>
+      {!!validationErrs ? <div>{validationErrs.errors}</div> : ''}
+    </div>
+  );
+}
+
+export default App;
